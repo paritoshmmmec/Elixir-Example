b30d7d5fb1a496cb6e7afe5909ed01a70cf3383d
paritosh baghel <paritoshmmmec@gmail.com>
2019-03-16 19:08:21 -0400

    Adding comments and code

--- hello.exs
+++ hello.exs
@@ -1,3 +1,18 @@
-IO.puts("hello from world")
+## Say Hello to elixir.
+IO.puts("Hello to elixir.")
 
+## There is no assignment operator , Only match operator `=`
+## Here right value will be matched against unbounded x to pass this now x have to be 1
+## so match operator
 x = 1
+
+## You can rebound values in elixir.
+x = 2
+
+## this code will throw MatchError no match of right hand side value: 2 Uncomment this line to see the real error
+## 3 = x
+
+IO.puts("Proving bool are actually ")
+IO.puts :true == true
+
+
