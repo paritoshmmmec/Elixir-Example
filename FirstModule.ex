03bd5f45a164d6cdb540a78396823aacb042f868
paritosh baghel <paritoshmmmec@gmail.com>
2019-04-05 18:56:26 -0400

    Readme and List

--- FirstModule.ex
+++ FirstModule.ex
@@ -1,18 +1,32 @@
-defmodule Shape do
-  def perimeter(r) do
-    2 * r
-  end
-  ## Arity is a fancy name for the number of arguments a function receives. A function is
-  ## uniquely identified by its containing module, its name, and its arity. Take a look at the
-  ## following function:
-  def arrow do
-    -5
-    |> abs
-    |> Integer.to_string()
-    |> IO.puts()
-  end
+# defmodule Shape do
+#   def perimeter(r) do
+#     2 * r
+#   end
 
-  def arrow2(r) do
+#   ## Arity is a fancy name for the number of arguments a function receives. A function is
+#   ## uniquely identified by its containing module, its name, and its arity. Take a look at the
+#   ## following function:
+#   def arrow do
+#     -5
+#     |> abs
+#     |> Integer.to_string()
+#     |> IO.puts()
+#   end
 
-  end
-end
+#   def arrow2(r) do
+#   end
+# end
+
+# defmodule SentenceParser do
+
+#   # takes a sentence and find unique character int it
+#   def convert(sentence) do
+#     sentence
+#     |> String.upcase()
+#     |> String.reverse()
+#     |> String.graphemes()
+#     |> Enum.sort()
+#    end
+# end
+
+# IO.puts(SentenceParser.convert("Paritosh is cool"))
---
+++ list.exs
@@ -0,0 +1,4 @@
+
+IO.inspect([1, :ok, "Hello"])
+IO.inspect([1, :ok, "Hello"] ++ ["tail"])
+IO.inspect(["head"] ++ [1, :ok, "Hello"])
